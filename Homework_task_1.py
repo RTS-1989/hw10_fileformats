8c9c04f792682218845dc08218af1ff16d6fbdef
RTS1989 <Russibg@gmail.com>
2020-06-02 00:13:05 +0000

    Изменил алгоритм подсчета. Теперь функция считает слова в разных регистрах. Плюс одну функцию импортировал из другого файла.

--- Homework_task_1.py
+++ Homework_task_1.py
@@ -1,4 +1,5 @@
-import json, collections
+import json
+import collections
 from collections import Counter
 
 def get_info(file_path) -> list:
@@ -19,23 +20,33 @@ def get_info(file_path) -> list:
 
     return news_lists
 
-def top_word_counter() -> str:
+def top_word_counter(top_figure: int) -> str:
 
     news_lists = get_info('newsafr.json')
 
     c = collections.Counter()
+    d = collections.Counter()
 
     for list_of_item in news_lists:
         for word in list_of_item:
             if len(word) > 6:
-                c[word] += 1
+                if word.islower():
+                    c[word] += 1
+                elif word.istitle():
+                    d[word] += 1
 
-    a = Counter(c).most_common(10)
+    top_list_lower = Counter(c).most_common(top_figure)
+    top_list_title = Counter(d).most_common(top_figure)
 
-    print('Список 10 наиболее часто употребляемых слов:')
+    print(f'Список {top_figure} наиболее часто употребляемых слов с маленькой буквы:')
 
-    for word_count_item in a:
-        print(f'{a.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
+    for word_count_item in top_list_lower:
+        print(f'{top_list_lower.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
+
+    print(f'\nСписок {top_figure} наиболее часто употребляемых слов с большой буквы:')
+
+    for word_count_item in top_list_title:
+        print(f'{top_list_title.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
 
 if __name__ == '__main__':
-    top_word_counter()
+    top_word_counter(10)
--- Homework_task_2.py
+++ Homework_task_2.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET, collections
-from collections import Counter
+from Homework_task_1 import top_word_counter
 
 def get_info(file_path) -> list:
     parser = ET.XMLParser(encoding="utf-8")
@@ -14,23 +14,5 @@ def get_info(file_path) -> list:
 
     return news_lists
 
-def top_word_counter() -> str:
-
-    news_lists = get_info("newsafr.xml")
-
-    c = collections.Counter()
-
-    for list_of_item in news_lists:
-        for word in list_of_item:
-            if len(word) > 6:
-                c[word] += 1
-
-    a = Counter(c).most_common(10)
-
-    print('Список 10 наиболее часто употребляемых слов:')
-
-    for word_count_item in a:
-        print(f'{a.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
-
 if __name__ == '__main__':
-    top_word_counter()
+    top_word_counter(10)
