1a756b9d9074d1b1149c8ad5f2eca732102f99ad
RTS1989 <Russibg@gmail.com>
2020-06-03 12:19:11 +0000

    Сейчас все в одном файле. Подсчет слов теперь в одном регистре. Функция для подсчета принимает теперь 3 аргумента.

--- main.py
+++ main.py
@@ -1,71 +1,60 @@
-import json
-import collections
+import json, xml.etree.ElementTree as ET, collections
 from collections import Counter
 
-def get_info(file_path) -> list:
+def get_info_json(file_path) -> list:
 
-    with open('newsafr.json', encoding='utf8') as read_file:
+    with open(file_path, encoding='utf8') as read_file:
         data = json.load(read_file)
 
     news_lists = []
 
-    for item in data.values():
-        if item['channel']:
-            for key, value in item['channel'].items():
-                if key == 'items':
-                    for info_dict in value:
-                        for key, value in info_dict.items():
-                            if key == 'description':
-                                news_lists.append(value.split())
+    for value_list in data['rss']['channel']['items']:
+        news_lists.append(value_list['description'].split())
 
     return news_lists
 
-def top_word_counter(top_figure: int) -> str:
+def get_info_xml(file_path) -> list:
+    
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser)
+    root = tree.getroot()
+    xml_items = root.findall("channel/item")
 
-    news_lists = get_info('newsafr.json')
+    news_lists = []
 
+    for item in xml_items:
+        news_lists.append(item.find('description').text.split())
+
+    return news_lists
+
+
+def top_word_counter(function, file_path, top_figure: int) -> str:
+
+    news_lists = function(file_path)
+    news_lists_word_lower = []
+    
     c = collections.Counter()
-    d = collections.Counter()
 
     for list_of_item in news_lists:
         for word in list_of_item:
-            if len(word) > 6:
-                if word.islower():
-                    c[word] += 1
-                elif word.istitle():
-                    d[word] += 1
+            if word.istitle():
+                lower_word = word.lower()
+                news_lists_word_lower.append(lower_word)
+            else:
+                news_lists_word_lower.append(word)
+
+    for word in news_lists_word_lower:
+        if len(word) > 6:
+            c[word] += 1
 
     top_list_lower = Counter(c).most_common(top_figure)
-    top_list_title = Counter(d).most_common(top_figure)
 
-    print(f'Список {top_figure} наиболее часто употребляемых слов с маленькой буквы:')
+    print(f'\nСписок {top_figure} наиболее часто употребляемых слов:')
 
     for word_count_item in top_list_lower:
         print(f'{top_list_lower.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
 
-    print(f'\nСписок {top_figure} наиболее часто употребляемых слов с большой буквы:')
-
-    for word_count_item in top_list_title:
-        print(f'{top_list_title.index(word_count_item) + 1} место у слова "{word_count_item[0]}" количество повторений равняется {word_count_item[-1]}')
 
 if __name__ == '__main__':
-    top_word_counter(10)
-
-# import xml.etree.ElementTree as ET, collections
-# from Homework_task_1 import top_word_counter
-
-# def get_info(file_path) -> list:
-#     parser = ET.XMLParser(encoding="utf-8")
-#     tree = ET.parse("newsafr.xml", parser)
-#     root = tree.getroot()
-#     xml_items = root.findall("channel/item")
-
-#     news_lists = []
-
-#     for item in xml_items:
-#         news_lists.append(item.find('description').text.split())
-
-#     return news_lists
-
-# if __name__ == '__main__':
-#     top_word_counter(10)
+    top_word_counter(get_info_xml, 'newsafr.xml', 10)
+    top_word_counter(get_info_json, 'newsafr.json', 10)
